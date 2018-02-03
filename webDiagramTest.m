0e030ca9757970cd77b93641e5511954ba5aaffa
Henry Tregillus <tonerboy@gmail.com>
2018-02-02 18:45:23 -0700

    Update webDiagramTest.m

--- webDiagramTest.m
+++ webDiagramTest.m
@@ -6,16 +6,24 @@
 % condition, then produces a matrix suitable for plotting a web diagram
 % using matlab's "quiver" plotting function.
 
-%% Usage
-% Define the function you're interested in:
 
+%%--------------------------------------------------------------------------
+% Usage: In general use, you only really need to change this stuff here.
+% Define the function you're interested in:
 g = @(x) 3.5.*x.*(1-x);
+% Set the phase space that the function resides in. This is generally R, but usually you only care about some small basin of attraction anyways.
 X = linspace(0,1,1000);
+% Pick an initial value
 x0 = .1;
+% Set how many iterates you want to do. You can make this number as big as you want, but at some point it becomes largely useless.
 steps = 10;
+%%--------------------------------------------------------------------------
+
+
+% Create the web diagram with the above inputs.
 web1 = webDiagram(g,X,x0,steps);
 
-%% Plotting the results
+%% Plot the results
 
 hold on
 quiver(X,X,web1(:,:,2),web1(:,:,1),0,'MaxHeadSize',.025)
@@ -23,7 +31,7 @@ plot(X,g(X))
 plot(X,X)
 ylim([min(X) max(X)])
 xlim([min(X) max(X)])
-title(['Web diagram for g(x) with ' num2str(steps) ' iterativess'])
+title(['Web diagram for g_{3.5}(x) with ' num2str(steps) ' iterativess'])
 xlabel('x')
 ylabel('g(x)')
 hold off
